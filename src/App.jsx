fa49992c8ec468557d2e0261af01b39af80e564c
taher ali <taheralishaikh146@gmail.com>
2025-08-24 19:00:05 +0530

    components and props'data passing'in cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Card from './components/Card.jsx'
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <button onClick={()=>{
-          setCount(count + )
-        }}>count is {count}</button>
-      </div>
-
+    <div className="cards">
+      <Card name = 'taher ali'/>
+      <Card name='shaktiman'/>
+      <Card name='hakla sharuk'/>
+    </div>
     </>
   )
 }
 
-export default App
+export default App;
