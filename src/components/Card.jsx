e0dc8ae7e8aa538515de184d7add7bb7fade535a
taher ali <taheralishaikh146@gmail.com>
2025-08-27 19:40:27 +0530

    props and usestate effect

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
-import vitelogo from '../assets/react.svg'
+import { useState } from 'react'
 import './Card.css'
 
 const Card = (props) => {
+
+  const [first, setfirst] = useState(0)
+  
   return (
     <>
     <div className='mm'>
-        <img src={vitelogo} alt="hh" />
+        <img src={props.imageSrc} alt="hh" />
         <div className="cont">
             <h1>{props.name}</h1>
             <p>hello, i am a web developer and i know html css javascript and currently learning react</p>
-            <button>contact</button>
+            <button onClick={ ()=>{
+              setfirst(first +1)
+            }}> click {first}</button>
         </div>
     </div>
     </>
